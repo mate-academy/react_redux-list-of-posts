fcea7a9dac0049cf21b6b59f01b673724904d2c1
IvanVFedenko <ivan.v.fedenko@gmail.com>
2019-08-19 15:40:56 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,30 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
 
-function App() {
+import './App.css';
+import {getData} from './Get';
+import PostList from './PostList';
+
+import {
+  setPosts,
+  getLoading,
+  getIsLoading,
+  getIsLoaded,
+  getLoadedPosts,
+  getPostTemplate,
+  setFilteredPosts,
+        } from './store';
+
+class App extends React.Component {
+
+handleLoad = async() => {
+  const { setPosts, getLoading} = this.props;
+  getLoading();
+  const postsWithUserAndComments = await getData();
+  setPosts(postsWithUserAndComments);
+};
+
+filteringPosts = (event) => {
+  const { setFilteredPosts } = this.props;
+  const { value } = event.target;
+  setFilteredPosts(value)
+};
+
+render() {
+  const { isLoaded, isLoading } = this.props
+  if (!isLoaded) {
+    return (
+      <button
+        className="app_load-button"
+        type="button"
+        onClick={this.handleLoad}
+        hidden={isLoaded}
+        disabled={isLoading}
+      >
+        {isLoading ? 'Loading...' : 'Load'}
+      </button>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div>
+        <label>
+          <input
+            type="text"
+            placeholder="Search post by body or title..."
+            onChange={this.filteringPosts}
+            className="app_search-input"
+          />
+        </label>
+      </div>
+      <PostList loadedPosts={this.props.loadedPosts} />
     </div>
   );
 }
+}
 
-export default App;
+const getProps = state => ({
+  loadedPosts: getLoadedPosts(state),
+  postTemplate: getPostTemplate(state),
+  isLoaded: getIsLoaded(state),
+  isLoading: getIsLoading(state),
+});
+
+const getMethods = dispatch => ({
+  getLoading: () => dispatch(getLoading()),
+  setPosts: (value) => dispatch(setPosts(value)),
+  setFilteredPosts: (value) => dispatch(setFilteredPosts(value)),
+});
+
+export default connect(getProps, getMethods)(App);
