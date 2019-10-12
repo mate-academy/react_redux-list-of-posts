bdb0d1b7717a59d1951dcb37ced46d320b7d5cd9
Pukhnichenko <victorpuhnichenko@gmail.com>
2019-10-12 22:09:21 +0300

    fixed_2

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedApp />
-  </Provider>, document.getElementById('root')
+  </Provider>,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
