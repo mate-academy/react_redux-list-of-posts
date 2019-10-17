d449ff2e24590f73ac1f1310cec970c8c7c21cfd
vladyslav223 <vladyslav223@gmail.com>
2019-10-17 11:49:44 +0300

    fixed

---
+++ src/components/Comment/Comment.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Comment.css';
+
+const Comment = ({ comment }) => {
+  const { name, email, body } = comment;
+
+  return (
+    <div className="comment">
+      <button type="button" className="remove comment-remove">✕</button>
+      <div className="comment__name">
+        { name.charAt(0).toUpperCase() + name.slice(1) }
+      </div>
+      <div className="comment__body">
+        { body.charAt(0).toUpperCase() + body.slice(1) }
+      </div>
+      <div className="comment__email">
+        { email.charAt(0).toUpperCase() + email.slice(1) }
+      </div>
+    </div>
+  );
+};
+
+export default Comment;
