29499e36d9c0b21c068b01674c280ea638e897af
natya2203 <nastya_2203@ukr.net>
2020-01-09 14:40:52 +0200

    solution

--- src/components/comment.js
+++ src/components/comment.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { deleteComment } from '../actions';
 import DeleteIcon from '@material-ui/icons/Delete';
+import { deleteComment } from '../actions';
 
 const SingleComment = ({ title, body, email, id, removeComment }) => (
   <section className="comment">
