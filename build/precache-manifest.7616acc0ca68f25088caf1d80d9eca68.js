981ec7c8802aabddf62d768caa6efad97b89fdd2
natya2203 <nastya_2203@ukr.net>
2020-01-09 11:51:59 +0200

    solution

---
+++ build/precache-manifest.7616acc0ca68f25088caf1d80d9eca68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34c69def4023b80295eb10a28e577919",
+    "url": "./index.html"
+  },
+  {
+    "revision": "033ff492a0b7309a3fa4",
+    "url": "./static/css/main.5f91e428.chunk.css"
+  },
+  {
+    "revision": "cc6771d3d742757e3227",
+    "url": "./static/js/2.2f44a619.chunk.js"
+  },
+  {
+    "revision": "033ff492a0b7309a3fa4",
+    "url": "./static/js/main.62b2c2ff.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
