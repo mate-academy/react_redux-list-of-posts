08da9cf33a1cb7864315d58ddfd31ead056f8107
Denys <denys.cheporniuk@gmail.com>
2020-01-10 19:27:17 +0200

    gh pages

---
+++ build/precache-manifest.995f850ed54cc5159d845311928d44f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4dcc0883a2bc006d5a5968a749026af",
+    "url": "./index.html"
+  },
+  {
+    "revision": "15e3d2beb7611aba1035",
+    "url": "./static/css/main.a52a443b.chunk.css"
+  },
+  {
+    "revision": "8d6374f26822b066ffeb",
+    "url": "./static/js/2.e2c5ec96.chunk.js"
+  },
+  {
+    "revision": "15e3d2beb7611aba1035",
+    "url": "./static/js/main.ef5cdffc.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
