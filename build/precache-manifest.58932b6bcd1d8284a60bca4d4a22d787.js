578a54f7d79c2716a8da4b2e1bca3a44cab5981f
Iuliia Tsentylo <labliuk.maksym@gmail.com>
2020-01-14 17:16:24 +0200

    solution

---
+++ build/precache-manifest.58932b6bcd1d8284a60bca4d4a22d787.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96d473fd6f6734c5e45afde83638b811",
+    "url": "./index.html"
+  },
+  {
+    "revision": "49c7acffef241d04fe2e",
+    "url": "./static/css/main.79380cad.chunk.css"
+  },
+  {
+    "revision": "0f51944dc7f7a029d1e4",
+    "url": "./static/js/2.06bd3581.chunk.js"
+  },
+  {
+    "revision": "49c7acffef241d04fe2e",
+    "url": "./static/js/main.b4ac8072.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
