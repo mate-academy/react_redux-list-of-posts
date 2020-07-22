262cd29d455e866bc98577995b604b4469a12c47
Liliia <chuiko1112@gmail.com>
2020-07-22 14:26:03 +0300

    solution

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -8,12 +8,12 @@ import { PostItem } from './PostItem';
 export const PostsList = () => {
   const posts = useSelector(getPosts);
   const option = useSelector(getFilterOption);
+  const visiblePosts = posts.filter(item => (item.title + item.body).includes(option));
 
   return (
     <ul className="posts">
       {
-        posts.filter(item => (item.title + item.body).includes(option))
-          .map(post => <PostItem key={post.id} post={post} />)
+        visiblePosts.map(post => <PostItem key={post.id} post={post} />)
       }
     </ul>
   );
