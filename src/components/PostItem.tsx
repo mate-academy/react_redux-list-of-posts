e0b88fe043d853600111f8be0dff09f63c054776
Yaroslav Andriyenko <andriyenko.yaroslav@gmail.com>
2020-07-30 13:09:06 +0300

    Delete button was added

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getPosts } from '../store';
+import { setPosts } from '../store/postsReducer';
 import { User, Post, Comment } from './Interfaces';
 import { CommentList } from './CommentList';
 import { UserItem } from './UserItem';
@@ -9,15 +12,37 @@ interface Props {
   post: Post;
 }
 
-export const PostItem: React.FC<Props> = ({ comments, user, post }) => (
-  <li className="post">
-    <h1>
-      {post.title}
-    </h1>
-    <p>
-      {post.body}
-    </p>
-    <UserItem user={user} />
-    <CommentList comments={comments} />
-  </li>
-);
+export const PostItem: React.FC<Props> = ({ comments, user, post }) => {
+  const dispatch = useDispatch();
+  const preparedPosts = useSelector(getPosts);
+
+  const handleClick = (id: number) => {
+    const newPosts = {
+      posts: preparedPosts.posts
+        .filter((item: Post) => item.id !== id),
+      users: [...preparedPosts.users],
+      comments: [...preparedPosts.comments],
+    };
+
+    dispatch(setPosts(newPosts));
+  };
+
+  return (
+    <li className="post">
+      <h1>
+        {post.title}
+      </h1>
+      <button
+        type="button"
+        onClick={() => handleClick(post.id)}
+      >
+        Delete
+      </button>
+      <p>
+        {post.body}
+      </p>
+      <UserItem user={user} />
+      <CommentList comments={comments} />
+    </li>
+  );
+};
