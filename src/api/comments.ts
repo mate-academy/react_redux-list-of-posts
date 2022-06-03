838b31d4b00ef318c25c1f4507a4cb9dcdd8acdc
Yegor Kochetkov <kegor.fx@gmail.com>
2022-06-03 14:58:45 +0300

    Add types & requests

---
+++ src/api/comments.ts
@@ -0,0 +1,33 @@
+import { BASE_URL } from './api';
+import { Comment } from '../types/Comment';
+import { NewComment } from '../types/NewComment';
+
+export const getPostComments = async (postId: number): Promise<Comment[]> => {
+  const response = await fetch(`${BASE_URL}/comments?postId=${postId}`);
+
+  return response.json();
+};
+
+export const removeComment = async (commentId: number): Promise<Comment> => {
+  const response = await fetch(
+    `${BASE_URL}/comments/${commentId}`,
+    { method: 'DELETE' },
+  );
+
+  return response.json();
+};
+
+export const addComment = async (newComment: NewComment): Promise<Comment> => {
+  const response = await fetch(
+    `${BASE_URL}/comments/`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify(newComment),
+    },
+  );
+
+  return response.json();
+};
