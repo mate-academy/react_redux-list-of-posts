1a70f59a372c9dc5a4f5d8c4b4f080b2a0febe4e
Yegor Kochetkov <kegor.fx@gmail.com>
2022-06-04 23:03:06 +0300

    Move selectors to separate file

---
+++ src/store/selectors.ts
@@ -0,0 +1,6 @@
+import { RootState } from './index';
+
+// Selectors - a function receiving Redux state and returning some data from it
+export const isLoading = (state: RootState) => state.loading;
+export const getMessage = (state: RootState) => state.message;
+export const getUserId = (state: RootState) => state.userId;
