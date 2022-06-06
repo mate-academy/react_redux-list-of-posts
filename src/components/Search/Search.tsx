2b9c3efbce2194dfb715388d39c747c3d584a5dd
Yegor Kochetkov <kegor.fx@gmail.com>
2022-06-06 17:11:32 +0300

    Add filter with debounce

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,37 +1,19 @@
 import { debounce } from 'lodash';
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setDisplayedPosts } from '../../store/displayedPosts';
-import { getPosts } from '../../store/selectors';
-import { Post } from '../../types/Post';
-// import { setUserId } from '../../store/user';
+import { useDispatch } from 'react-redux';
+import React, { useState } from 'react';
+import { setQuery } from '../../store/query';
 
 export const Search: React.FC = () => {
-  const posts: Post[] = useSelector(getPosts);
   const dispatch = useDispatch();
 
   const [titleQuery, setTitleQuery] = useState('');
-  const [appliedQuery, setAppliedQuery] = useState('');
-
-  const applyQuery = debounce(setAppliedQuery, 1000);
+  const delay = 1000;
+  const applyQuery = debounce((query) => dispatch(setQuery(query)), delay);
 
   const handleQuery = (query: string) => {
     setTitleQuery(query);
     applyQuery(query);
   };
-
-  useEffect(() => {
-    if (posts) {
-      const visibleTodos = [...posts].filter((post) => {
-        const isQuery = post.title.toLowerCase()
-          .includes(appliedQuery.toLowerCase());
-
-        return isQuery;
-      });
-
-      dispatch(setDisplayedPosts(visibleTodos));
-    }
-  }, [appliedQuery, posts]);
 
   return (
     <label>
