4a650827ed20ff75a8dc2d62558264154d26aa4b
KristinaTar <kristina.tarashchuk@gmail.com>
2022-06-24 20:27:50 +0300

    deleted comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,3 @@
-// import { useSelector } from 'react-redux';
-
-// import './App.scss';
-// import { Start } from './components/Start';
-
-// import { isLoading, getMessage } from './store';
-
-// const App = () => {
-//   const loading = useSelector(isLoading);
-//   const message = useSelector(getMessage) || 'Ready!';
-
-//   return (
-//     <div className="App">
-//       <h1>Redux list of posts</h1>
-//       <h2>{loading ? 'Loading...' : message}</h2>
-
-//       <Start />
-//     </div>
-//   );
-// };
-
-// export default App;
-
 import React, { useEffect } from 'react';
 import './App.scss';
 import './styles/general.scss';
--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,8 @@
-// import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux';
-// import { BrowserRouter } from 'react-router-dom';
-
-// import App from './App';
-// import store from './store';
-
-// const Root = () => (
-//   <Provider store={store}>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </Provider>
-// );
-
-// ReactDOM.render(<Root />, document.getElementById('root'));
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import App from './App';
-import store from './redux';
+import store from './redux/index';
 
 ReactDOM.render(
   <Provider store={store}>
