cb7b458a53bb9ae44928eacd3663a25dccf01cc4
mhrynko <glukym@gmail.com>
2022-09-02 11:09:29 +0300

    use new cypress-tools

--- src/features/counter/counterSlice.ts
+++ src/features/counter/counterSlice.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 // eslint-disable-next-line import/no-cycle
 import { RootState, AppThunk } from '../../app/store';
