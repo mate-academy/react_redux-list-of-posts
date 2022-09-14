781a3c2aedf6fbb6b92d7363024c95e411a35201
sergii-nosachenko <s.nosachenko@gmail.com>
2022-09-14 15:17:49 +0300

    Fixed line length issue

--- src/api/comments.ts
+++ src/api/comments.ts
@@ -6,8 +6,8 @@ export const getPostComments = (postId: number) => (
   client.get<IComment[]>(`/comments?postId=${postId}`)
 );
 
-// eslint-disable-next-line max-len
-export type TCommentToPost = Pick<IComment, 'name' | 'email' | 'body' | 'postId'>;
+type TCommentFields = 'name' | 'email' | 'body' | 'postId';
+export type TCommentToPost = Pick<IComment, TCommentFields>;
 
 export const postNewComment = (comment: TCommentToPost) => (
   client.post<IComment>('/comments', comment)
