0e0c55d233e79b26e47f8e409f7d977013841a1f
E Fernandez <ernesto.fernandez.1980@gmail.com>
2022-10-25 11:53:45 +0300

    ver1

--- src/App.tsx
+++ src/App.tsx
@@ -48,12 +48,84 @@ export const App: React.FC = () => {
       {/* Learn the Redux Toolkit usage example in src/app and src/features/counter */}
       <Counter />
 
+      <div className="container" style={{ border: '3px solid red;' }}>
+        <div className="tile is-ancestor">
+          <div className="tile is-parent">
+            <div className="tile is-child box is-success">
+              <div className="block">
+                <UserSelector
+                  value={author}
+                  onChange={setAuthor}
+                />
+              </div>
+
+              <div className="block" data-cy="MainContent">
+                {!author && (
+                  <p data-cy="NoSelectedUser">
+                    No user selected
+                  </p>
+                )}
+
+                {author && !loaded && (
+                  <Loader />
+                )}
+
+                {author && loaded && hasError && (
+                  <div
+                    className="notification is-danger"
+                    data-cy="PostsLoadingError"
+                  >
+                    Something went wrong!
+                  </div>
+                )}
+
+                {author && loaded && !hasError && posts.length === 0 && (
+                  <div className="notification is-warning" data-cy="NoPostsYet">
+                    No posts yet
+                  </div>
+                )}
+
+                {author && loaded && !hasError && posts.length > 0 && (
+                  <PostsList
+                    posts={posts}
+                    selectedPostId={selectedPost?.id}
+                    onPostSelected={setSelectedPost}
+                  />
+                )}
+              </div>
+            </div>
+          </div>
+
+          <div
+            data-cy="Sidebar"
+            className={classNames(
+              'tile',
+              'is-parent',
+              'is-8-desktop',
+              'Sidebar',
+              {
+                'Sidebar--open': selectedPost,
+              },
+            )}
+          >
+            <div className="tile is-child box is-success ">
+              {selectedPost && (
+                <PostDetails post={selectedPost} />
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div className="container">
         <div className="tile is-ancestor">
           <div className="tile is-parent">
             <div className="tile is-child box is-success">
               <div className="block">
-                <UserSelector value={author} onChange={setAuthor} />
+                <UserSelector
+                  value={author}
+                  onChange={setAuthor}
+                />
               </div>
 
               <div className="block" data-cy="MainContent">
