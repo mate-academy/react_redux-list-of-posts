835db0d6efb4aec6cb61cd5117478c64a621b3aa
Ksenia Didukh <k.tsviatko@gmail.com>
2022-11-30 11:43:56 +0200

    Solution

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,20 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import authorSlice from '../features/authorSlice';
+import commentsSlice from '../features/commentsSlice';
 // eslint-disable-next-line import/no-cycle
 import counterReducer from '../features/counter/counterSlice';
+import postsSlice from '../features/postsSlice';
+import selectedPostSlice from '../features/selectedPostSlice';
+import userSlice from '../features/userSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    users: userSlice,
+    author: authorSlice,
+    posts: postsSlice,
+    selectedPost: selectedPostSlice,
+    comments: commentsSlice,
   },
 });
 
---
+++ src/features/authorSlice.ts
@@ -0,0 +1,25 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { User } from '../types/User';
+
+type AuthorState = {
+  author: User | null,
+};
+
+const initialState: AuthorState = {
+  author: null,
+};
+
+const authorSlice = createSlice({
+  name: 'author',
+  initialState,
+  reducers: {
+    setAuthor: (state, action: PayloadAction<User>) => {
+      // eslint-disable-next-line no-param-reassign
+      state.author = action.payload;
+    },
+  },
+});
+
+export default authorSlice.reducer;
+
+export const { setAuthor } = authorSlice.actions;
