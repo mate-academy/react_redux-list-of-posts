a2027032c5de3adbe81802d828eee37982b327cd
Artur Nagaycev <artur.nagaycev@gmail.com>
2023-01-06 12:43:48 +0200

    added authorSlice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,12 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 // eslint-disable-next-line import/no-cycle
 import counterReducer from '../features/counter/counterSlice';
+import authorReducer from '../features/mainApp/authorSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    author: authorReducer,
   },
 });
 
