dd06db9313d9f6584b199b4cf6af672b021abeda
Oleh Khashchevskyi <o.khashchevskyi@gmail.com>
2023-01-09 23:55:42 +0200

    solution

--- src/components/NewCommentForm.tsx
+++ src/components/NewCommentForm.tsx
@@ -1,181 +1,126 @@
-import classNames from 'classnames';
 import React, { useState } from 'react';
-import { CommentData } from '../types/Comment';
+import classNames from 'classnames';
+import { Controller, useForm } from 'react-hook-form';
+import { useAppDispatch, useAppSelector } from 'hooks/useRedux';
+import CommentsAsync from 'store/comments/commentsAsync';
+import { UiActions } from 'store/ui/uiSlice';
+import { selectSelectedPost } from 'store/posts/postsSelectors';
+import Post from 'models/Post';
+import { isEmailValid, isFieldRequired } from 'utilities/Validation';
+import Input from 'components/Controls/Input';
+import Textarea from './Controls/Textarea';
 
-type Props = {
-  onSubmit: (data: CommentData) => Promise<void>;
-};
+interface Form {
+  name: string,
+  email: string,
+  body: string,
+}
 
-export const NewCommentForm: React.FC<Props> = ({ onSubmit }) => {
-  const [submitting, setSubmitting] = useState(false);
+export const NewCommentForm: React.FC = () => {
+  const dispatch = useAppDispatch();
 
-  const [errors, setErrors] = useState({
-    name: false,
-    email: false,
-    body: false,
+  const selectedPost: Post | null = useAppSelector(selectSelectedPost);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const {
+    handleSubmit, control, reset,
+    formState: { errors },
+  } = useForm<Form>({
+    defaultValues: {
+      name: '',
+      email: '',
+      body: '',
+    },
   });
 
-  const [{ name, email, body }, setValues] = useState({
-    name: '',
-    email: '',
-    body: '',
-  });
-
-  const clearForm = () => {
-    setValues({
+  const handleReset = () => {
+    reset({
       name: '',
       email: '',
       body: '',
     });
-
-    setErrors({
-      name: false,
-      email: false,
-      body: false,
-    });
   };
 
-  const handleChange = (
-    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
-    const { name: field, value } = event.target;
-
-    setValues(current => ({ ...current, [field]: value }));
-    setErrors(current => ({ ...current, [field]: false }));
-  };
-
-  const handleSubmit = async (event: React.FormEvent) => {
-    event.preventDefault();
-
-    setErrors({
-      name: !name,
-      email: !email,
-      body: !body,
-    });
-
-    if (!name || !email || !body) {
+  const onSubmit = handleSubmit((data: Form) => {
+    if (!selectedPost) {
       return;
     }
 
-    setSubmitting(true);
+    setLoading(true);
+    const nexData = { ...data, postId: selectedPost?.id };
 
-    // it is very easy to forget about `await` keyword
-    await onSubmit({ name, email, body });
-
-    // and the spinner will disappear immediately
-    setSubmitting(false);
-    setValues(current => ({ ...current, body: '' }));
-    // We keep the entered name and email
-  };
+    dispatch(CommentsAsync.createComment(nexData))
+      .unwrap()
+      .then(() => {
+        handleReset();
+        dispatch(UiActions.enqueueSnackbar({
+          message: 'Comment has been added',
+        }));
+      })
+      .finally(() => setLoading(false));
+  });
 
   return (
-    <form onSubmit={handleSubmit} onReset={clearForm} data-cy="NewCommentForm">
-      <div className="field" data-cy="NameField">
-        <label className="label" htmlFor="comment-author-name">
-          Author Name
-        </label>
-
-        <div className="control has-icons-left has-icons-right">
-          <input
-            type="text"
-            name="name"
-            id="comment-author-name"
+    <form
+      onSubmit={onSubmit}
+      noValidate
+      data-cy="NewCommentForm"
+    >
+      <Controller
+        control={control}
+        name="name"
+        rules={{ required: isFieldRequired }}
+        render={({ field: { onChange, value } }) => (
+          <Input
+            label="Author Name"
             placeholder="Name Surname"
-            className={classNames('input', { 'is-danger': errors.name })}
-            value={name}
-            onChange={handleChange}
+            value={value}
+            onChange={onChange}
+            required
+            error={!!errors.name}
+            errorText={errors.name?.message || ''}
           />
-
-          <span className="icon is-small is-left">
-            <i className="fas fa-user" />
-          </span>
-
-          {errors.name && (
-            <span
-              className="icon is-small is-right has-text-danger"
-              data-cy="ErrorIcon"
-            >
-              <i className="fas fa-exclamation-triangle" />
-            </span>
-          )}
-        </div>
-
-        {errors.name && (
-          <p className="help is-danger" data-cy="ErrorMessage">
-            Name is required
-          </p>
         )}
-      </div>
-
-      <div className="field" data-cy="EmailField">
-        <label className="label" htmlFor="comment-author-email">
-          Author Email
-        </label>
-
-        <div className="control has-icons-left has-icons-right">
-          <input
-            type="text"
-            name="email"
-            id="comment-author-email"
+      />
+      <Controller
+        control={control}
+        name="email"
+        rules={{ required: isFieldRequired, pattern: isEmailValid }}
+        render={({ field: { onChange, value } }) => (
+          <Input
+            label="Author Email"
             placeholder="email@test.com"
-            className={classNames('input', { 'is-danger': errors.email })}
-            value={email}
-            onChange={handleChange}
+            value={value}
+            onChange={onChange}
+            required
+            error={!!errors.email}
+            errorText={errors.email?.message || ''}
           />
-
-          <span className="icon is-small is-left">
-            <i className="fas fa-envelope" />
-          </span>
-
-          {errors.email && (
-            <span
-              className="icon is-small is-right has-text-danger"
-              data-cy="ErrorIcon"
-            >
-              <i className="fas fa-exclamation-triangle" />
-            </span>
-          )}
-        </div>
-
-        {errors.email && (
-          <p className="help is-danger" data-cy="ErrorMessage">
-            Email is required
-          </p>
         )}
-      </div>
-
-      <div className="field" data-cy="BodyField">
-        <label className="label" htmlFor="comment-body">
-          Comment Text
-        </label>
-
-        <div className="control">
-          <textarea
-            id="comment-body"
-            name="body"
+      />
+      <Controller
+        control={control}
+        name="body"
+        rules={{ required: isFieldRequired }}
+        render={({ field: { onChange, value } }) => (
+          <Textarea
+            label="Comment Text"
             placeholder="Type comment here"
-            className={classNames('textarea', { 'is-danger': errors.body })}
-            value={body}
-            onChange={handleChange}
+            value={value}
+            onChange={onChange}
+            required
+            error={!!errors.body}
+            errorText={errors.body?.message || ''}
           />
-        </div>
-
-        {errors.body && (
-          <p
-            className="help is-danger"
-            data-cy="ErrorMessage"
-          >
-            Enter some text
-          </p>
         )}
-      </div>
+      />
 
       <div className="field is-grouped">
         <div className="control">
           <button
             type="submit"
             className={classNames('button', 'is-link', {
-              'is-loading': submitting,
+              'is-loading': loading,
             })}
           >
             Add
@@ -183,8 +128,11 @@ export const NewCommentForm: React.FC<Props> = ({ onSubmit }) => {
         </div>
 
         <div className="control">
-          {/* eslint-disable-next-line react/button-has-type */}
-          <button type="reset" className="button is-link is-light">
+          <button
+            type="button"
+            onClick={handleReset}
+            className="button is-link is-light"
+          >
             Clear
           </button>
         </div>
---
+++ src/utilities/Validation.ts
@@ -0,0 +1,9 @@
+export const isFieldRequired = {
+  value: true,
+  message: 'This field is required',
+};
+
+export const isEmailValid = {
+  value: /\S+@\S+\.\S+/,
+  message: 'E-mail is not valid',
+};
