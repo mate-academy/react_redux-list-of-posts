2785ff943d48781b2661555ee910407859263eba
Denys Kleimenov <denyskleimenovit@gmail.com>
2023-01-10 11:54:24 +0200

    List of posts was created

--- src/components/PostDetails.tsx
+++ src/components/PostDetails.tsx
@@ -5,56 +5,39 @@ import { NewCommentForm } from './NewCommentForm';
 import * as commentsApi from '../api/comments';
 
 import { Post } from '../types/Post';
-import { Comment, CommentData } from '../types/Comment';
+import { CommentData } from '../types/Comment';
+import { useAppDispatch, useAppSelector } from '../app/hooks';
+import { actions as commentsActions } from '../features/comments';
 
 type Props = {
   post: Post;
 };
 
 export const PostDetails: React.FC<Props> = ({ post }) => {
-  const [comments, setComments] = useState<Comment[]>([]);
-  const [loaded, setLoaded] = useState(false);
-  const [hasError, setError] = useState(false);
+  const dispatch = useAppDispatch();
+  const { items, loaded, hasError } = useAppSelector((state) => state.comments);
+  const comments = items;
   const [visible, setVisible] = useState(false);
 
-  function loadComments() {
-    setLoaded(false);
-    setError(false);
-    setVisible(false);
-
-    commentsApi.getPostComments(post.id)
-      .then(setComments) // save the loaded comments
-      .catch(() => setError(true)) // show an error when something went wrong
-      .finally(() => setLoaded(true)); // hide the spinner
-  }
-
-  useEffect(loadComments, [post.id]);
-
-  // The same useEffect with async/await
-  /*
   async function loadComments() {
-    setLoaded(false);
+    dispatch(commentsActions.setLoaded(false));
+    dispatch(commentsActions.setError(false));
     setVisible(false);
-    setError(false);
 
     try {
       const commentsFromServer = await commentsApi.getPostComments(post.id);
 
-      setComments(commentsFromServer);
+      dispatch(commentsActions.set(commentsFromServer));
     } catch (error) {
-      setError(true);
+      dispatch(commentsActions.setError(true));
     } finally {
-      setLoaded(true);
+      dispatch(commentsActions.setLoaded(true));
     }
-  };
+  }
 
   useEffect(() => {
     loadComments();
-  }, []);
-
-  useEffect(loadComments, [post.id]); // Wrong!
-  // effect can return only a function but not a Promise
-  */
+  }, [post.id]);
 
   const addComment = async ({ name, email, body }: CommentData) => {
     try {
@@ -65,48 +48,29 @@ export const PostDetails: React.FC<Props> = ({ post }) => {
         postId: post.id,
       });
 
-      setComments(
-        currentComments => [...currentComments, newComment],
-      );
-
-      // setComments([...comments, newComment]);
-      // works wrong if we wrap `addComment` with `useCallback`
-      // because it takes the `comments` cached during the first render
-      // not the actual ones
+      dispatch(commentsActions.add(newComment));
     } catch (error) {
-      // we show an error message in case of any error
-      setError(true);
+      dispatch(commentsActions.setError(true));
     }
   };
 
   const deleteComment = async (commentId: number) => {
     // we delete the comment immediately so as
     // not to make the user wait long for the actual deletion
-    setComments(
-      currentComments => currentComments.filter(
-        comment => comment.id !== commentId,
-      ),
-    );
-
+    dispatch(commentsActions.delete(commentId));
     await commentsApi.deleteComment(commentId);
   };
 
   return (
     <div className="content" data-cy="PostDetails">
       <div className="block">
-        <h2 data-cy="PostTitle">
-          {`#${post.id}: ${post.title}`}
-        </h2>
+        <h2 data-cy="PostTitle">{`#${post.id}: ${post.title}`}</h2>
 
-        <p data-cy="PostBody">
-          {post.body}
-        </p>
+        <p data-cy="PostBody">{post.body}</p>
       </div>
 
       <div className="block">
-        {!loaded && (
-          <Loader />
-        )}
+        {!loaded && <Loader />}
 
         {loaded && hasError && (
           <div className="notification is-danger" data-cy="CommentsError">
@@ -124,7 +88,7 @@ export const PostDetails: React.FC<Props> = ({ post }) => {
           <>
             <p className="title is-4">Comments:</p>
 
-            {comments.map(comment => (
+            {comments.map((comment) => (
               <article
                 className="message is-small"
                 key={comment.id}
--- src/components/UserSelector.tsx
+++ src/components/UserSelector.tsx
@@ -1,35 +1,34 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import classNames from 'classnames';
-import { UserContext } from './UsersContext';
 import { User } from '../types/User';
+import { useAppDispatch, useAppSelector } from '../app/hooks';
+import { actions as usersActions } from '../features/users';
+import { getUsers } from '../api/users';
+import { actions as authorActions } from '../features/author';
 
 type Props = {
   value: User | null;
-  onChange: (user: User) => void;
 };
 
 export const UserSelector: React.FC<Props> = ({
-  // `value` and `onChange` are traditional names for the form field
-  // `selectedUser` represents what actually stored here
   value: selectedUser,
-  onChange,
 }) => {
-  // `users` are loaded from the API, so for the performance reasons
-  // we load them once in the `UsersContext` when the `App` is opened
-  // and now we can easily reuse the `UserSelector` in any form
-  const users = useContext(UserContext);
-  const [expanded, setExpanded] = useState(false);
+  const dispatch = useAppDispatch();
+  const { users, expanded } = useAppSelector((state) => state.users);
+
+  useEffect(() => {
+    getUsers().then((usersFromServer: User[]) => {
+      dispatch(usersActions.set(usersFromServer));
+    });
+  }, []);
 
   useEffect(() => {
     if (!expanded) {
       return;
     }
 
-    // we save a link to remove the listener later
     const handleDocumentClick = () => {
-      // we close the Dropdown on any click (inside or outside)
-      // So there is not need to check if we clicked inside the list
-      setExpanded(false);
+      dispatch(usersActions.setExpanded(false));
     };
 
     document.addEventListener('click', handleDocumentClick);
@@ -38,8 +37,6 @@ export const UserSelector: React.FC<Props> = ({
     return () => {
       document.removeEventListener('click', handleDocumentClick);
     };
-  // we don't want to listening for outside clicks
-  // when the Dopdown is closed
   }, [expanded]);
 
   return (
@@ -54,12 +51,10 @@ export const UserSelector: React.FC<Props> = ({
           aria-haspopup="true"
           aria-controls="dropdown-menu"
           onClick={() => {
-            setExpanded(current => !current);
+            dispatch(usersActions.setExpanded(!expanded));
           }}
         >
-          <span>
-            {selectedUser?.name || 'Choose a user'}
-          </span>
+          <span>{selectedUser?.name || 'Choose a user'}</span>
 
           <span className="icon is-small">
             <i className="fas fa-angle-down" aria-hidden="true" />
@@ -69,12 +64,12 @@ export const UserSelector: React.FC<Props> = ({
 
       <div className="dropdown-menu" id="dropdown-menu" role="menu">
         <div className="dropdown-content">
-          {users.map(user => (
+          {users.map((user) => (
             <a
               key={user.id}
               href={`#user-${user.id}`}
               onClick={() => {
-                onChange(user);
+                dispatch(authorActions.set(user));
               }}
               className={classNames('dropdown-item', {
                 'is-active': user.id === selectedUser?.id,
