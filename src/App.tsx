ea0522147d3c446a148bbbbc3ef70d7c162fe424
Artem Hirzhev <a.hirzhev@gmail.com>
2023-02-11 12:39:28 +0200

    Remove Counter

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import { PostsList } from './components/PostsList';
 import { PostDetails } from './components/PostDetails';
 import { UserSelector } from './components/UserSelector';
 import { Loader } from './components/Loader';
-import { Counter } from './features/counter/Counter';
 import { useAppDispatch, useAppSelector } from './app/hooks';
 import { initUsers } from './features/usersSlice';
 import { initPosts, set as setPosts } from './features/postsSlice';
@@ -38,8 +37,6 @@ export const App: React.FC = () => {
 
   return (
     <main className="section">
-      <Counter />
-
       <div className="container">
         <div className="tile is-ancestor">
           <div className="tile is-parent">
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,5 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import authorReducer from '../features/authorSlice';
-// eslint-disable-next-line import/no-cycle
-import counterReducer from '../features/counter/counterSlice';
 import usersReducer from '../features/usersSlice';
 import postsReducer from '../features/postsSlice';
 import selectedPostReducer from '../features/selectedPostSlice';
@@ -9,7 +7,6 @@ import commentsReducer from '../features/commentsSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     users: usersReducer,
     author: authorReducer,
     posts: postsReducer,
