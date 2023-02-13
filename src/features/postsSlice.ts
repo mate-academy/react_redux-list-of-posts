b2be51b55317181704d7aeaeabbefe29f76e56b7
Nurdan Okcu Tanich <nurokcudan@gmail.com>
2023-02-13 14:15:22 +0100

    posts with redux

---
+++ src/features/postsSlice.ts
@@ -0,0 +1,51 @@
+/* eslint-disable no-param-reassign */
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { Post } from '../types/Post';
+import { getUserPosts } from '../api/posts';
+
+export interface PostsState {
+  posts: Post[];
+  status: 'idle' | 'loading' | 'failed';
+  error: boolean;
+}
+const initialState: PostsState = {
+  posts: [],
+  status: 'idle',
+  error: false,
+};
+
+export const loadPosts = createAsyncThunk(
+  'posts/SET',
+  (async (userId: number) => {
+    const response = await getUserPosts(userId);
+
+    return response;
+  }),
+);
+
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {
+    clearPosts(state) {
+      state.posts = [];
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(loadPosts.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(loadPosts.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.posts = action.payload;
+      })
+      .addCase(loadPosts.rejected, (state) => {
+        state.status = 'failed';
+        state.error = true;
+      });
+  },
+});
+
+export const { clearPosts } = postsSlice.actions;
+export default postsSlice.reducer;
