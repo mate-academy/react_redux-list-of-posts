c82be27cec30d2200541e0101e6e482b00690cf6
Oleh Savchuk <sav4uk.oleg@gmail.com>
2023-03-09 01:46:39 +0200

    fixed

---
+++ src/features/postsSlice.ts
@@ -0,0 +1,57 @@
+/* eslint-disable no-param-reassign */
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { getUserPosts } from '../api/posts';
+import { Post } from '../types/Post';
+
+export const loadUsersPosts = createAsyncThunk(
+  'posts/fetch',
+  (authorId: number) => getUserPosts(authorId),
+);
+
+type PostsState = {
+  posts: Post[],
+  loaded: boolean,
+  hasError: boolean,
+};
+
+const initialState: PostsState = {
+  posts: [],
+  loaded: false,
+  hasError: false,
+};
+
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {
+    set: (state, action: PayloadAction<Post[]>) => {
+      state.posts = action.payload;
+    },
+    setLoaded: (state, action: PayloadAction<boolean>) => {
+      state.loaded = action.payload;
+    },
+    setError: (state, action: PayloadAction<boolean>) => {
+      state.loaded = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(loadUsersPosts.pending, (state) => {
+      state.loaded = false;
+    });
+
+    builder.addCase(loadUsersPosts.fulfilled, (state, action) => {
+      state.loaded = true;
+      state.hasError = false;
+      state.posts = action.payload;
+    });
+
+    builder.addCase(loadUsersPosts.rejected, (state) => {
+      state.hasError = true;
+      state.posts = [];
+      state.loaded = true;
+    });
+  },
+});
+
+export default postsSlice.reducer;
+export const { set } = postsSlice.actions;
