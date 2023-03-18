4af3284f2ed729477d5bc01ff06fca0cdb0cdae5
Vladimir Yurkov <vladimir.yurkov.91@gmail.com>
2023-03-18 14:58:31 +0200

    requested changes fix

--- src/components/PostDetails.tsx
+++ src/components/PostDetails.tsx
@@ -24,32 +24,6 @@ export const PostDetails: React.FC = () => {
 
   useEffect(loadComments, [selectedPost?.id]);
 
-  // The same useEffect with async/await
-  /*
-  async function loadComments() {
-    setLoaded(false);
-    setVisible(false);
-    setError(false);
-
-    try {
-      const commentsFromServer = await commentsApi.getPostComments(post.id);
-
-      setComments(commentsFromServer);
-    } catch (error) {
-      setError(true);
-    } finally {
-      setLoaded(true);
-    }
-  };
-
-  useEffect(() => {
-    loadComments();
-  }, []);
-
-  useEffect(loadComments, [post.id]); // Wrong!
-  // effect can return only a function but not a Promise
-  */
-
   const addComment = async ({ name, email, body }: CommentData) => {
     try {
       if (selectedPost) {
