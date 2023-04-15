187c5a099c65abbffdb55ec5d02fe55bedab7fe8
ctk12 <chetankatwa12@gmail.com>
2023-04-15 12:29:11 +0530

    Added React Redux List of Posts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@mate-academy/eslint-config-react-typescript', 'plugin:cypress/recommended'],
   rules: {
-    'no-param-reassign': ["error", { "props": true, "ignorePropertyModificationsFor": ["state"] }],
+    'no-param-reassign': 0,
     'max-len': ['error', {
       ignoreTemplateLiterals: true,
       ignoreComments: true,
