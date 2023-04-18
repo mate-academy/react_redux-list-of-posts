c89accbd27de2c3f8e145bc8f1cdca0365faa557
@nataliia.vyrsta <natali.vyrsta@gmail.com>
2023-04-18 06:47:16 +0300

    first commit

---
+++ src/features/usersSlice.ts
@@ -0,0 +1,24 @@
+/* eslint-disable no-param-reassign */
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { getUsers } from '../api/users';
+import { User } from '../types/User';
+
+type Props = { items: User[] };
+
+const initialState: Props = { items: [] };
+
+export const loadApiUsers = createAsyncThunk('users/load', getUsers);
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {},
+
+  extraReducers: (builder) => {
+    builder.addCase(loadApiUsers.fulfilled, (state, action) => {
+      state.items = action.payload;
+    });
+  },
+});
+
+export default usersSlice.reducer;
