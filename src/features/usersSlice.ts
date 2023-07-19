58d038fb6b05640b8fbbb4532218baa15e8b6486
Andrii Kovalskyi <andrii.kovalskyi02@gmail.com>
2023-07-19 21:17:17 +0200

    solution

---
+++ src/features/usersSlice.ts
@@ -0,0 +1,30 @@
+/* eslint-disable no-param-reassign */
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { getUsers } from '../api/users';
+import { User } from '../types/User';
+
+type UsersState = {
+  users: User[];
+};
+
+const initialState: UsersState = {
+  users: [],
+};
+
+export const usersAsync = createAsyncThunk(
+  'users/fetchUsers',
+  getUsers,
+);
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(usersAsync.fulfilled, (state, action) => {
+      state.users = action.payload;
+    });
+  },
+});
+
+export default usersSlice.reducer;
