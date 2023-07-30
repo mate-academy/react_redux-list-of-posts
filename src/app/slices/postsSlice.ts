80d56289d4e59b16881056d7e2a5743fb69283eb
diamat1997 <diamat1997@gmail.com>
2023-07-30 17:21:36 +0300

    add task solution

---
+++ src/app/slices/postsSlice.ts
@@ -0,0 +1,45 @@
+/* eslint-disable no-param-reassign */
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Post } from '../../types/Post';
+import { getUserPosts } from '../../api/posts';
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts',
+  async (userId: number) => {
+    const fetchedPosts = await getUserPosts(userId);
+
+    return fetchedPosts;
+  });
+
+export interface PostsState {
+  loaded: boolean;
+  hasError: boolean;
+  items: Post[];
+}
+
+const initialState: PostsState = {
+  loaded: false,
+  hasError: false,
+  items: [],
+};
+
+export const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchPosts.pending, (state: PostsState) => {
+        state.loaded = false;
+        state.hasError = false;
+      })
+      .addCase(fetchPosts.fulfilled,
+        (state: PostsState, action: PayloadAction<Post[]>) => {
+          state.loaded = true;
+          state.items = action.payload;
+        })
+      .addCase(fetchPosts.rejected, (state: PostsState) => {
+        state.loaded = true;
+        state.hasError = true;
+      });
+  },
+});
--- src/components/PostDetails.tsx
+++ src/components/PostDetails.tsx
@@ -1,105 +1,47 @@
 import React, { useEffect, useState } from 'react';
 import { Loader } from './Loader';
 import { NewCommentForm } from './NewCommentForm';
+import { useAppDispatch, useAppSelector } from '../app/hooks';
+import {
+  commentsSlice,
+  fetchComments,
+  removeComment,
+} from '../app/slices/commentsSlice';
 
-import * as commentsApi from '../api/comments';
-
-import { Post } from '../types/Post';
-import { Comment, CommentData } from '../types/Comment';
-
-type Props = {
-  post: Post;
-};
-
-export const PostDetails: React.FC<Props> = ({ post }) => {
-  const [comments, setComments] = useState<Comment[]>([]);
-  const [loaded, setLoaded] = useState(false);
-  const [hasError, setError] = useState(false);
+export const PostDetails: React.FC = () => {
   const [visible, setVisible] = useState(false);
-
-  function loadComments() {
-    setLoaded(false);
-    setError(false);
-    setVisible(false);
-
-    commentsApi.getPostComments(post.id)
-      .then(setComments) // save the loaded comments
-      .catch(() => setError(true)) // show an error when something went wrong
-      .finally(() => setLoaded(true)); // hide the spinner
-  }
-
-  useEffect(loadComments, [post.id]);
-
-  // The same useEffect with async/await
-  /*
-  async function loadComments() {
-    setLoaded(false);
-    setVisible(false);
-    setError(false);
-
-    try {
-      const commentsFromServer = await commentsApi.getPostComments(post.id);
-
-      setComments(commentsFromServer);
-    } catch (error) {
-      setError(true);
-    } finally {
-      setLoaded(true);
-    }
-  };
+  const {
+    hasError,
+    items: comments,
+    loaded,
+  } = useAppSelector(state => state.comments);
+  const { selectedPost } = useAppSelector(state => state.selectedPost);
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
-    loadComments();
-  }, []);
-
-  useEffect(loadComments, [post.id]); // Wrong!
-  // effect can return only a function but not a Promise
-  */
-
-  const addComment = async ({ name, email, body }: CommentData) => {
-    try {
-      const newComment = await commentsApi.createComment({
-        name,
-        email,
-        body,
-        postId: post.id,
-      });
-
-      setComments(
-        currentComments => [...currentComments, newComment],
-      );
-
-      // setComments([...comments, newComment]);
-      // works wrong if we wrap `addComment` with `useCallback`
-      // because it takes the `comments` cached during the first render
-      // not the actual ones
-    } catch (error) {
-      // we show an error message in case of any error
-      setError(true);
+    if (selectedPost) {
+      dispatch(fetchComments(selectedPost.id));
     }
-  };
+
+    setVisible(false);
+  }, [selectedPost]);
 
   const deleteComment = async (commentId: number) => {
-    // we delete the comment immediately so as
-    // not to make the user wait long for the actual deletion
-    setComments(
-      currentComments => currentComments.filter(
-        comment => comment.id !== commentId,
-      ),
-    );
-
-    await commentsApi.deleteComment(commentId);
+    dispatch(commentsSlice.actions.removeComment(commentId));
+    dispatch(removeComment(commentId));
   };
+
+  const { id, title, body } = selectedPost || {};
 
   return (
     <div className="content" data-cy="PostDetails">
       <div className="block">
         <h2 data-cy="PostTitle">
-          {`#${post.id}: ${post.title}`}
+          {`#${id}: ${title}`}
         </h2>
 
         <p data-cy="PostBody">
-          {post.body}
+          {body}
         </p>
       </div>
 
@@ -124,33 +66,39 @@ export const PostDetails: React.FC<Props> = ({ post }) => {
           <>
             <p className="title is-4">Comments:</p>
 
-            {comments.map(comment => (
-              <article
-                className="message is-small"
-                key={comment.id}
-                data-cy="Comment"
-              >
-                <div className="message-header">
-                  <a href={`mailto:${comment.email}`} data-cy="CommentAuthor">
-                    {comment.name}
-                  </a>
+            {comments.map(comment => {
+              const {
+                email, name, id: commentId, body: commentBody,
+              } = comment;
 
-                  <button
-                    data-cy="CommentDelete"
-                    type="button"
-                    className="delete is-small"
-                    aria-label="delete"
-                    onClick={() => deleteComment(comment.id)}
-                  >
-                    delete button
-                  </button>
-                </div>
+              return (
+                <article
+                  className="message is-small"
+                  key={commentId}
+                  data-cy="Comment"
+                >
+                  <div className="message-header">
+                    <a href={`mailto:${email}`} data-cy="CommentAuthor">
+                      {name}
+                    </a>
 
-                <div className="message-body" data-cy="CommentBody">
-                  {comment.body}
-                </div>
-              </article>
-            ))}
+                    <button
+                      data-cy="CommentDelete"
+                      type="button"
+                      className="delete is-small"
+                      aria-label="delete"
+                      onClick={() => deleteComment(commentId)}
+                    >
+                      delete button
+                    </button>
+                  </div>
+
+                  <div className="message-body" data-cy="CommentBody">
+                    {commentBody}
+                  </div>
+                </article>
+              );
+            })}
           </>
         )}
 
@@ -165,8 +113,8 @@ export const PostDetails: React.FC<Props> = ({ post }) => {
           </button>
         )}
 
-        {loaded && !hasError && visible && (
-          <NewCommentForm onSubmit={addComment} />
+        {selectedPost && loaded && !hasError && visible && (
+          <NewCommentForm />
         )}
       </div>
     </div>
