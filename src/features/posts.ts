816c5c9ec43bf7f6bf33c2ce50df078653ee801d
Liliia Danylenko <liliya.danylenko96@gmail.com>
2023-09-02 21:29:38 +0300

    solution

---
+++ src/features/posts.ts
@@ -0,0 +1,36 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { Post } from '../types/Post';
+import { getUserPosts } from '../api/posts';
+
+type InitialState = {
+  loaded: boolean,
+  hasError: boolean,
+  items: Post[];
+};
+
+const initialState: InitialState = {
+  loaded: true,
+  hasError: false,
+  items: [],
+};
+
+export const init = createAsyncThunk('posts/fetch', (id: number) => {
+  return getUserPosts(id);
+});
+
+export const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(init.pending, (state) => {
+      return { ...state, loaded: false };
+    });
+    builder.addCase(init.fulfilled, (state, action) => {
+      return { ...state, loaded: true, items: action.payload };
+    });
+    builder.addCase(init.rejected, (state) => {
+      return { ...state, loaded: true, hasError: true };
+    });
+  },
+});
