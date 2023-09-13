51d9c55a83bf3ef2ed1419acf6bed1b4ded30b50
Olena Boldysheva <adv55577@gmail.com>
2023-09-14 00:18:22 +0300

    add solution

---
+++ src/app/slices/index.ts
@@ -0,0 +1,12 @@
+import usersReducer from './usersSlice';
+import authorReducer from './authorSlice';
+import postsReducer from './postsSlice';
+import commentsReducer from './commentsSlice';
+
+// prettier-ignore
+export {
+  usersReducer,
+  authorReducer,
+  postsReducer,
+  commentsReducer,
+};
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,17 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-// eslint-disable-next-line import/no-cycle
-import counterReducer from '../features/counter/counterSlice';
+import {
+  usersReducer,
+  authorReducer,
+  postsReducer,
+  commentsReducer,
+} from './slices';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    users: usersReducer,
+    author: authorReducer,
+    posts: postsReducer,
+    comments: commentsReducer,
   },
 });
 
