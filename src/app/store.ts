36e445d847186892c1ca40afe00391847b9133af
Nigga <yomzfk@gmail.com>
2023-09-17 12:57:39 +0300

    solution

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,4 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-// eslint-disable-next-line import/no-cycle
 import counterReducer from '../features/counter/counterSlice';
 import usersReducer from '../features/usersSlice';
 import authorReducer from '../features/authorSlice';
@@ -21,11 +20,9 @@ export const store = configureStore({
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 
-/* eslint-disable @typescript-eslint/indent */
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
   unknown,
   Action<string>
 >;
-/* eslint-enable @typescript-eslint/indent */
