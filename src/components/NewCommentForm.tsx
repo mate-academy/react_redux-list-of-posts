f2342fd591f5c4bca8726ab4e04e9185a2b81f86
Mykola Plekan <zikzar@gmail.com>
2023-09-25 18:29:35 +0300

    solution

--- src/components/NewCommentForm.tsx
+++ src/components/NewCommentForm.tsx
@@ -1,13 +1,13 @@
 import classNames from 'classnames';
 import React, { useState } from 'react';
-import { CommentData } from '../types/Comment';
+import { useAppDispatch, useAppSelector } from '../app/hooks';
+import * as commentActions from '../features/comments';
 
-type Props = {
-  onSubmit: (data: CommentData) => Promise<void>;
-};
-
-export const NewCommentForm: React.FC<Props> = ({ onSubmit }) => {
+export const NewCommentForm: React.FC = () => {
   const [submitting, setSubmitting] = useState(false);
+
+  const dispatch = useAppDispatch();
+  const { selectedPost } = useAppSelector(state => state.selectedPost);
 
   const [errors, setErrors] = useState({
     name: false,
@@ -59,13 +59,15 @@ export const NewCommentForm: React.FC<Props> = ({ onSubmit }) => {
 
     setSubmitting(true);
 
-    // it is very easy to forget about `await` keyword
-    await onSubmit({ name, email, body });
+    await dispatch(commentActions.createNewComment({
+      name,
+      email,
+      body,
+      postId: selectedPost?.id || 0,
+    }));
 
-    // and the spinner will disappear immediately
     setSubmitting(false);
     setValues(current => ({ ...current, body: '' }));
-    // We keep the entered name and email
   };
 
   return (
---
+++ src/features/users.ts
@@ -0,0 +1,31 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { User } from '../types/User';
+import { getUsers } from '../api/users';
+
+type UsersState = {
+  items: User[];
+};
+
+const initialState: UsersState = {
+  items: [],
+};
+
+export const init = createAsyncThunk('users/fetch', () => {
+  return getUsers();
+});
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(init.fulfilled, (state, action) => {
+      return {
+        ...state,
+        items: action.payload,
+      };
+    });
+  },
+});
+
+export default usersSlice.reducer;
