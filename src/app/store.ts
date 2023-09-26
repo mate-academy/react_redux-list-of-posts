1f8ae0211fadf084baf32e434e15d3b0db41162f
Valera Viachalo <valeravyachalo@gmail.com>
2023-09-26 16:46:20 +0300

    .

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,5 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 // eslint-disable-next-line import/no-cycle
-import counterReducer from '../features/counter/counterSlice';
 import userSlice from '../features/userSlice';
 import authorSlice from '../features/authorSlice';
 import postsSlice from '../features/postsSlice';
@@ -9,7 +8,6 @@ import commentsSlice from '../features/commentsSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     users: userSlice,
     author: authorSlice,
     posts: postsSlice,
