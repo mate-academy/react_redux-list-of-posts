d25fbbc5ae7484923cd6b551cc1d0b3d75e2195d
zipic <zpc23041988@gmail.com>
2023-09-28 13:32:39 +0300

    redux-list-of-posts

--- src/components/UserSelector.tsx
+++ src/components/UserSelector.tsx
@@ -1,23 +1,12 @@
-import React, { useContext, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import classNames from 'classnames';
-import { UserContext } from './UsersContext';
-import { User } from '../types/User';
+import { useAppDispatch, useAppSelector } from '../app/hooks';
+import { setAuthor } from '../features/authorSlice';
 
-type Props = {
-  value: User | null;
-  onChange: (user: User) => void;
-};
-
-export const UserSelector: React.FC<Props> = ({
-  // `value` and `onChange` are traditional names for the form field
-  // `selectedUser` represents what actually stored here
-  value: selectedUser,
-  onChange,
-}) => {
-  // `users` are loaded from the API, so for the performance reasons
-  // we load them once in the `UsersContext` when the `App` is opened
-  // and now we can easily reuse the `UserSelector` in any form
-  const users = useContext(UserContext);
+export const UserSelector = () => {
+  const users = useAppSelector(state => state.users.items);
+  const selectedUser = useAppSelector(state => state.author.author);
+  const dispatch = useAppDispatch();
   const [expanded, setExpanded] = useState(false);
 
   useEffect(() => {
@@ -74,7 +63,9 @@ export const UserSelector: React.FC<Props> = ({
               key={user.id}
               href={`#user-${user.id}`}
               onClick={() => {
-                onChange(user);
+                setTimeout(() => {
+                  dispatch(setAuthor(user));
+                }, 1000);
               }}
               className={classNames('dropdown-item', {
                 'is-active': user.id === selectedUser?.id,
