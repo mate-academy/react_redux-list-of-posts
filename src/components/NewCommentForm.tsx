908281fe18264275515b940bb50723ebba516a32
Hishchak Tetiana <tanaiosered@ukr.net>
2023-11-19 21:50:28 +0200

    change functionnewComment

--- src/components/NewCommentForm.tsx
+++ src/components/NewCommentForm.tsx
@@ -40,7 +40,7 @@ export const NewCommentForm: React.FC<Props> = ({ onSubmit }) => {
   ) => {
     const { name: field, value } = event.target;
 
-    setValues(current => ({ ...current, [field]: value.trim() }));
+    setValues(current => ({ ...current, [field]: value }));
     setErrors(current => ({ ...current, [field]: false }));
   };
 
@@ -48,12 +48,12 @@ export const NewCommentForm: React.FC<Props> = ({ onSubmit }) => {
     event.preventDefault();
 
     setErrors({
-      name: !name,
-      email: !email,
-      body: !body,
+      name: !name.trim(),
+      email: !email.trim(),
+      body: !body.trim(),
     });
 
-    if (!name || !email || !body) {
+    if (!name.trim() || !email.trim() || !body.trim()) {
       return;
     }
 
