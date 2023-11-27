057f2aeecf93b399900717e26e38746d25c3567e
Hubert Wiktor <hubertwiktor6@gmail.com>
2023-11-27 19:49:20 +0100

    *

--- src/components/NewCommentForm.tsx
+++ src/components/NewCommentForm.tsx
@@ -60,12 +60,10 @@ export const NewCommentForm: React.FC<Props> = ({ onSubmit }) => {
     setSubmitting(true);
 
     // it is very easy to forget about `await` keyword
-    await onSubmit({ name, email, body });
-
-    // and the spinner will disappear immediately
-    setSubmitting(false);
-    setValues(current => ({ ...current, body: '' }));
-    // We keep the entered name and email
+    await onSubmit({ name, email, body }).then(() => {
+      setSubmitting(false);
+      setValues(current => ({ ...current, body: '' }));
+    });
   };
 
   return (
