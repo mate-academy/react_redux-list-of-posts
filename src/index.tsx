ff5bdbd9381e824ec0cc38ad7bbc4e3484765114
Victor Hrygorash <viktor12041991@i.ua>
2023-12-07 16:41:49 +0200

    done

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { HashRouter as Router } from 'react-router-dom';
 
 import { store } from './app/store';
 import { App } from './App';
 import { UsersProvider } from './components/UsersContext';
-
-const container = document.getElementById('root') as HTMLElement;
-const root = createRoot(container);
 
 const Root = () => (
   <Provider store={store}>
@@ -20,4 +17,4 @@ const Root = () => (
   </Provider>
 );
 
-root.render(<Root />);
+ReactDOM.render(<Root />, document.getElementById('root'));
