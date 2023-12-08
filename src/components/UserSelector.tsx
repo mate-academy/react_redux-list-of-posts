22b79514b266b4166b1a3bf8eff822433aac95a9
Nazar Kolomiets <lexnazar91@gmail.com>
2023-12-08 12:19:09 +0300

    add task solution 1

--- src/components/UserSelector.tsx
+++ src/components/UserSelector.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from 'react';
 import classNames from 'classnames';
 import { useAppDispatch, useAppSelector } from '../app/hooks';
