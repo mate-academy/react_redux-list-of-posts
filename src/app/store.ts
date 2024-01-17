22bae0aa94434528c077989fd2939af4adef2fb8
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-01-17 15:35:07 +0100

    implement user data fetching

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,12 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 // eslint-disable-next-line import/no-cycle
 import counterReducer from '../features/counter/counterSlice';
+import usersSliceReducer from '../features/users-slice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    users: usersSliceReducer,
   },
 });
 
---
+++ src/features/users-slice.ts
@@ -0,0 +1,33 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { getUsers } from '../api/users';
+import { User } from '../types/User';
+
+export interface T {
+  users: User[],
+}
+
+const initialState: T = {
+  users: [],
+};
+
+export const init = createAsyncThunk('users/loadUser',
+  async () => {
+    const users = await getUsers();
+
+    return users;
+  });
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {
+  },
+  /* eslint-disable no-param-reassign */
+  extraReducers: (builder) => {
+    builder.addCase(init.fulfilled, (state, action) => {
+      state.users = action.payload;
+    });
+  },
+});
+
+export default usersSlice.reducer;
