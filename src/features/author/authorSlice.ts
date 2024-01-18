8f2fab979d44d83d93173bdfbbd6bacf420281f5
Andriy Fesych <andriyhimself@gmail.com>
2024-01-19 01:37:57 +0200

    save_01

---
+++ src/features/author/authorSlice.ts
@@ -0,0 +1,23 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { User } from '../../types/User';
+
+export type AuthorState = {
+  author: User | null
+};
+
+const initialState: AuthorState = {
+  author: null,
+};
+
+export const authorSlice = createSlice({
+  name: 'author',
+  initialState,
+  reducers: {
+    set: (_state, action: PayloadAction<User>) => {
+      return { author: action.payload };
+    },
+  },
+});
+
+export const { actions } = authorSlice;
+export default authorSlice.reducer;
