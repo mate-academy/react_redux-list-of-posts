15c7ce080938248823030937c2707611fc06e671
2pasha <kostishinpavlo24@gmail.com>
2024-01-27 19:05:27 +0200

    action with comments was reimplemented with Redux

--- src/components/PostDetails.tsx
+++ src/components/PostDetails.tsx
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Loader } from './Loader';
-// import { NewCommentForm } from './NewCommentForm';
-
-// import * as commentsApi from '../api/comments';
+import { NewCommentForm } from './NewCommentForm';
 
 import { Post } from '../types/Post';
-// import { CommentData } from '../types/Comment';
+import { CommentData } from '../types/Comment';
 import { useAppDispatch, useAppSelector } from '../app/hooks';
 import * as CommentsActions from '../features/comments/commentsSlice';
 
@@ -14,95 +12,35 @@ type Props = {
 };
 
 export const PostDetails: React.FC<Props> = ({ post }) => {
+  const dispatch = useAppDispatch();
   const {
     comments,
     loaded,
     hasError,
   } = useAppSelector(state => state.comments);
-  const dispatch = useAppDispatch();
 
-  // const [comments, setComments] = useState<Comment[]>([]);
-  // const [loaded, setLoaded] = useState(false);
-  // const [hasError, setError] = useState(false);
   const [visible, setVisible] = useState(false);
-
-  // function loadComments() {
-  //   setLoaded(false);
-  //   setError(false);
-  //   setVisible(false);
-
-  //   commentsApi.getPostComments(post.id)
-  //     .then(setComments) // save the loaded comments
-  //     .catch(() => setError(true)) // show an error when something went wrong
-  //     .finally(() => setLoaded(true)); // hide the spinner
-  // }
 
   useEffect(() => {
     dispatch(CommentsActions.init(post.id));
   }, [post, dispatch]);
 
-  // useEffect(loadComments, [post.id]);
+  const addComment = async ({ name, email, body }: CommentData) => {
+    const newComment = {
+      name,
+      email,
+      body,
+      postId: post.id,
+    };
 
-  // The same useEffect with async/await
-  /*
-  async function loadComments() {
-    setLoaded(false);
-    setVisible(false);
-    setError(false);
-
-    try {
-      const commentsFromServer = await commentsApi.getPostComments(post.id);
-
-      setComments(commentsFromServer);
-    } catch (error) {
-      setError(true);
-    } finally {
-      setLoaded(true);
-    }
+    await dispatch(CommentsActions.addComment(newComment));
   };
 
-  useEffect(() => {
-    loadComments();
-  }, []);
+  const deleteComment = async (commentId: number) => {
+    dispatch(CommentsActions.remove(commentId));
 
-  useEffect(loadComments, [post.id]); // Wrong!
-  // effect can return only a function but not a Promise
-  */
-
-  // const addComment = async ({ name, email, body }: CommentData) => {
-  //   try {
-  //     const newComment = await commentsApi.createComment({
-  //       name,
-  //       email,
-  //       body,
-  //       postId: post.id,
-  //     });
-
-  //     setComments(
-  //       currentComments => [...currentComments, newComment],
-  //     );
-
-  //     // setComments([...comments, newComment]);
-  //     // works wrong if we wrap `addComment` with `useCallback`
-  //     // because it takes the `comments` cached during the first render
-  //     // not the actual ones
-  //   } catch (error) {
-  //     // we show an error message in case of any error
-  //     setError(true);
-  //   }
-  // };
-
-  // const deleteComment = async (commentId: number) => {
-  //   // we delete the comment immediately so as
-  //   // not to make the user wait long for the actual deletion
-  //   setComments(
-  //     currentComments => currentComments.filter(
-  //       comment => comment.id !== commentId,
-  //     ),
-  //   );
-
-  //   await commentsApi.deleteComment(commentId);
-  // };
+    await dispatch(CommentsActions.removeComment(commentId));
+  };
 
   return (
     <div className="content" data-cy="PostDetails">
@@ -153,7 +91,7 @@ export const PostDetails: React.FC<Props> = ({ post }) => {
                     type="button"
                     className="delete is-small"
                     aria-label="delete"
-                    // onClick={() => deleteComment(comment.id)}
+                    onClick={() => deleteComment(comment.id)}
                   >
                     delete button
                   </button>
@@ -178,9 +116,9 @@ export const PostDetails: React.FC<Props> = ({ post }) => {
           </button>
         )}
 
-        {/* {loaded && !hasError && visible && (
+        {loaded && !hasError && visible && (
           <NewCommentForm onSubmit={addComment} />
-        )} */}
+        )}
       </div>
     </div>
   );
--- src/features/comments/commentsSlice.ts
+++ src/features/comments/commentsSlice.ts
@@ -1,7 +1,11 @@
 /* eslint no-param-reassign: "error" */
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { Comment } from '../../types/Comment';
-import { getPostComments } from '../../api/comments';
+import {
+  getPostComments,
+  createComment,
+  deleteComment,
+} from '../../api/comments';
 
 type CommentsState = {
   loaded: boolean;
@@ -22,10 +26,30 @@ export const init = createAsyncThunk(
   },
 );
 
+export const addComment = createAsyncThunk(
+  'comments/add',
+  (data: Omit<Comment, 'id'>) => {
+    return createComment(data);
+  },
+);
+
+export const removeComment = createAsyncThunk(
+  'comments/delete',
+  (commentId: number) => {
+    return deleteComment(commentId);
+  },
+);
+
 const commentsSlice = createSlice({
   name: 'comments',
   initialState,
-  reducers: {},
+  reducers: {
+    remove: (state, action) => {
+      state.comments = state.comments.filter(
+        commment => commment.id !== action.payload,
+      );
+    },
+  },
   extraReducers(builder) {
     builder.addCase(init.pending, (state) => {
       state.loaded = false;
@@ -41,7 +65,23 @@ const commentsSlice = createSlice({
       state.loaded = true;
       state.hasError = true;
     });
+
+    builder.addCase(addComment.pending, (state) => {
+      state.loaded = false;
+      state.hasError = false;
+    });
+
+    builder.addCase(addComment.fulfilled, (state, action) => {
+      state.loaded = true;
+      state.comments = [...state.comments, action.payload];
+    });
+
+    builder.addCase(addComment.rejected, state => {
+      state.loaded = true;
+      state.hasError = true;
+    });
   },
 });
 
 export default commentsSlice.reducer;
+export const { remove } = commentsSlice.actions;
