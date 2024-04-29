71801a0f9f676eac3b970ed1f15bba2e000e33ca
Bohdan Antipenko <82728301+ANTI-bogdan@users.noreply.github.com>
2024-04-29 17:36:50 +0300

    fixing2

--- src/components/NewCommentForm.tsx
+++ src/components/NewCommentForm.tsx
@@ -48,12 +48,12 @@ export const NewCommentForm: React.FC<Props> = ({ onSubmit }) => {
     event.preventDefault();
 
     setErrors({
-      name: !name,
-      email: !email,
+      name: !name.trim(),
+      email: !email.trim(),
       body: !body.trim(),
     });
 
-    if (!name || !email || !body.trim()) {
+    if (!name.trim() || !email.trim() || !body.trim()) {
       return;
     }
 
