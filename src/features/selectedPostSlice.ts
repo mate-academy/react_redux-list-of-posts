d89dfdf7c891beb2135d5dcb78b72b9519444f9a
Maria Snegireva <mariyasn88@gmail.com>
2024-05-17 18:10:38 +0300

    fixed mistakes

--- src/features/selectedPostSlice.ts
+++ src/features/selectedPostSlice.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-param-reassign */
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { Post } from '../types/Post';
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Post } from "../types/Post";
 
 type SelectedPost = {
   post: Post | null;
