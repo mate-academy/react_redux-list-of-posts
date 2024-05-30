d6da393253fb0afd7f961032c4616f084c2ecf3b
volodymyr <volodiayurovych@gmail.com>
2024-05-30 13:10:21 +0200

    sol

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,4 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 // eslint-disable-next-line import/no-cycle
 import usersReducer from '../features/usersSlice';
 import userReducer from '../features/authorSlice';
@@ -18,3 +18,12 @@ export const store = configureStore({
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
+
+/* eslint-disable @typescript-eslint/indent */
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
+/* eslint-enable @typescript-eslint/indent */
