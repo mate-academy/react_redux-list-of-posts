062e0c4158429f21dcd578a83564dc76fe178792
Yevhenii <polotnyak2004@gmail.com>
2024-06-22 17:37:27 +0300

    sol

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,20 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 // eslint-disable-next-line import/no-cycle
 import counterReducer from '../features/counter/counterSlice';
+import usersReducer from '../features/users';
+import comentsReducer from '../features/coments';
+import postsReducer from '../features/posts';
+import selectedPostReducer from '../features/selectedPost';
+import authorReducer from '../features/author';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    users: usersReducer,
+    comments: comentsReducer,
+    posts: postsReducer,
+    selectedPost: selectedPostReducer,
+    author: authorReducer,
   },
 });
 
