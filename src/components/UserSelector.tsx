1c4efb73684d44a5230bf925720675ad9d0a56a5
Anastasia Granovskaya <iamgranovskaya@gmail.com>
2024-09-12 17:56:12 +0300

    solution

--- src/components/UserSelector.tsx
+++ src/components/UserSelector.tsx
@@ -1,24 +1,15 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import classNames from 'classnames';
-import { UserContext } from './UsersContext';
-import { User } from '../types/User';
+import { useAppDispatch, useAppSelector } from '../app/hooks';
+import * as usersActions from '../features/users';
+import * as authorActions from '../features/author';
+import * as selectedPostActions from '../features/selectedPost';
 
-type Props = {
-  value: User | null;
-  onChange: (user: User) => void;
-};
-
-export const UserSelector: React.FC<Props> = ({
-  // `value` and `onChange` are traditional names for the form field
-  // `selectedUser` represents what actually stored here
-  value: selectedUser,
-  onChange,
-}) => {
-  // `users` are loaded from the API, so for the performance reasons
-  // we load them once in the `UsersContext` when the `App` is opened
-  // and now we can easily reuse the `UserSelector` in any form
-  const users = useContext(UserContext);
+export const UserSelector: React.FC = () => {
   const [expanded, setExpanded] = useState(false);
+  const { users } = useAppSelector(state => state.users);
+  const { author } = useAppSelector(state => state.author);
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
     if (!expanded) {
@@ -42,6 +33,10 @@ export const UserSelector: React.FC<Props> = ({
     // when the Dopdown is closed
   }, [expanded]);
 
+  useEffect(() => {
+    dispatch(usersActions.init());
+  }, [dispatch]);
+
   return (
     <div
       data-cy="UserSelector"
@@ -58,7 +53,7 @@ export const UserSelector: React.FC<Props> = ({
             setExpanded(current => !current);
           }}
         >
-          <span>{selectedUser?.name || 'Choose a user'}</span>
+          <span>{author?.name || 'Choose a user'}</span>
 
           <span className="icon is-small">
             <i className="fas fa-angle-down" aria-hidden="true" />
@@ -73,10 +68,11 @@ export const UserSelector: React.FC<Props> = ({
               key={user.id}
               href={`#user-${user.id}`}
               onClick={() => {
-                onChange(user);
+                dispatch(selectedPostActions.set(null))
+                  dispatch(authorActions.set(user));
               }}
               className={classNames('dropdown-item', {
-                'is-active': user.id === selectedUser?.id,
+                'is-active': user.id === author?.id,
               })}
             >
               {user.name}
---
+++ src/features/author.ts
@@ -0,0 +1,24 @@
+/* eslint-disable no-param-reassign */
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { User } from '../types/User';
+
+type AuthorState = {
+  author: User | null;
+};
+
+const initialState: AuthorState = {
+  author: null,
+};
+
+export const authorSlice = createSlice({
+  name: 'author',
+  initialState,
+  reducers: {
+    set: (state, action: PayloadAction<User>) => {
+      state.author = action.payload;
+    },
+  },
+});
+
+export default authorSlice.reducer;
+export const { set } = authorSlice.actions;
