ecdb3ce5301cd10d138b8c46109d08458ec902f7
Andrii Zakharenko <andriizakharenko@gmail.com>
2024-09-30 18:05:59 +0300

    feat: updated isLoading (PostDetails)

--- src/components/PostDetails.tsx
+++ src/components/PostDetails.tsx
@@ -45,60 +45,65 @@ export const PostDetails: React.FC<Props> = ({ post }) => {
       </div>
 
       <div className="block">
-        {isLoading && <Loader />}
-
-        {!isLoading && error && (
-          <div className="notification is-danger" data-cy="CommentsError">
-            Something went wrong
-          </div>
-        )}
-        {!isLoading && !error && comments.length === 0 && (
-          <p className="title is-4" data-cy="NoCommentsMessage">
-            No comments yet
-          </p>
-        )}
-        {!isLoading && !error && comments.length > 0 && (
+        {isLoading ? (
+          <Loader />
+        ) : (
           <>
-            <p className="title is-4">Comments:</p>
-            {comments.map(comment => (
-              <article
-                className="message is-small"
-                key={comment.id}
-                data-cy="Comment"
-              >
-                <div className="message-header">
-                  <a href={`mailto:${comment.email}`} data-cy="CommentAuthor">
-                    {comment.name}
-                  </a>
-                  <button
-                    data-cy="CommentDelete"
-                    type="button"
-                    className="delete is-small"
-                    aria-label="delete"
-                    onClick={() => deleteComment(comment.id)}
+            {error && (
+              <div className="notification is-danger" data-cy="CommentsError">
+                Something went wrong
+              </div>
+            )}
+            {!error && comments.length === 0 && (
+              <p className="title is-4" data-cy="NoCommentsMessage">
+                No comments yet
+              </p>
+            )}
+            {!error && comments.length > 0 && (
+              <>
+                <p className="title is-4">Comments:</p>
+                {comments.map(comment => (
+                  <article
+                    className="message is-small"
+                    key={comment.id}
+                    data-cy="Comment"
                   >
-                    delete button
-                  </button>
-                </div>
-                <div className="message-body" data-cy="CommentBody">
-                  {comment.body}
-                </div>
-              </article>
-            ))}
+                    <div className="message-header">
+                      <a
+                        href={`mailto:${comment.email}`}
+                        data-cy="CommentAuthor"
+                      >
+                        {comment.name}
+                      </a>
+                      <button
+                        data-cy="CommentDelete"
+                        type="button"
+                        className="delete is-small"
+                        aria-label="delete"
+                        onClick={() => deleteComment(comment.id)}
+                      >
+                        delete button
+                      </button>
+                    </div>
+                    <div className="message-body" data-cy="CommentBody">
+                      {comment.body}
+                    </div>
+                  </article>
+                ))}
+              </>
+            )}
+            {!error && !visible && (
+              <button
+                data-cy="WriteCommentButton"
+                type="button"
+                className="button is-link"
+                onClick={() => setVisible(true)}
+              >
+                Write a comment
+              </button>
+            )}
+            {!error && visible && <NewCommentForm onSubmit={addComment} />}
           </>
-        )}
-        {!isLoading && !error && !visible && (
-          <button
-            data-cy="WriteCommentButton"
-            type="button"
-            className="button is-link"
-            onClick={() => setVisible(true)}
-          >
-            Write a comment
-          </button>
-        )}
-        {!isLoading && !error && visible && (
-          <NewCommentForm onSubmit={addComment} />
         )}
       </div>
     </div>
