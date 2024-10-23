9950ef2362ff2793c0f3fdfe8c2cde01c9e0764e
Pavlo Pukalo <misterkatlet@gmail.com>
2024-10-23 19:35:13 +0300

    solution

--- src/components/UserSelector.tsx
+++ src/components/UserSelector.tsx
@@ -1,23 +1,19 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import classNames from 'classnames';
-import { UserContext } from './UsersContext';
 import { User } from '../types/User';
+import { useAppDispatch, useAppSelector } from '../app/hooks';
+import * as usersActions from '../features/users';
+import { setAuthor } from '../features/author';
 
-type Props = {
-  value: User | null;
-  onChange: (user: User) => void;
-};
+export const UserSelector: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const selectedUser = useAppSelector(state => state.author.author);
 
-export const UserSelector: React.FC<Props> = ({
-  // `value` and `onChange` are traditional names for the form field
-  // `selectedUser` represents what actually stored here
-  value: selectedUser,
-  onChange,
-}) => {
-  // `users` are loaded from the API, so for the performance reasons
-  // we load them once in the `UsersContext` when the `App` is opened
-  // and now we can easily reuse the `UserSelector` in any form
-  const users = useContext(UserContext);
+  useEffect(() => {
+    dispatch(usersActions.set());
+  }, []);
+
+  const users = useAppSelector(state => state.users.users);
   const [expanded, setExpanded] = useState(false);
 
   useEffect(() => {
@@ -41,6 +37,10 @@ export const UserSelector: React.FC<Props> = ({
     // we don't want to listening for outside clicks
     // when the Dopdown is closed
   }, [expanded]);
+
+  const onUserChange = (user: User) => {
+    dispatch(setAuthor(user));
+  };
 
   return (
     <div
@@ -73,7 +73,7 @@ export const UserSelector: React.FC<Props> = ({
               key={user.id}
               href={`#user-${user.id}`}
               onClick={() => {
-                onChange(user);
+                onUserChange(user);
               }}
               className={classNames('dropdown-item', {
                 'is-active': user.id === selectedUser?.id,
