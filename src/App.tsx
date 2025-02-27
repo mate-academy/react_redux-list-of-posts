2a67b558fde15a44cde54f8a60a696997007ed1e
Oleksii_Mac <olshum8@gmail.com>
2025-02-27 17:28:21 +0200

    task solution after test

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ export const App: React.FC = () => {
     }
 
     dispatch(clearCurrentPost());
-  }, [author, dispatch]);
+  }, [author, dispatch, loadUserPosts]);
 
   return (
     <main className="section">
