5303dc3a09067842d3b0660a4825fb10800927b4
Mykola Melnichuk <nikolfutdeveloper@gmail.com>
2025-04-11 20:01:50 +0200

    add solution

---
+++ src/app/slices/selectedPostSlice.ts
@@ -0,0 +1,21 @@
+/* eslint-disable no-param-reassign */
+
+import { createSlice } from '@reduxjs/toolkit';
+import { Post } from '../../types/Post';
+
+const initialState = {
+  selectedPost: null as Post | null,
+};
+
+export const selectedPostSlice = createSlice({
+  name: 'selectedPost',
+  initialState,
+  reducers: {
+    setSelectedPost(state, action) {
+      state.selectedPost = action.payload;
+    },
+  },
+});
+
+export const { setSelectedPost } = selectedPostSlice.actions;
+export default selectedPostSlice.reducer;
