9489d33a637ebcc61f92fabedbbe7efb6c93035a
Natalia <natalia.23santos@hotmail.com>
2025-04-11 23:47:02 -0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,39 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import classNames from 'classnames';
 
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
-
 import { PostsList } from './components/PostsList';
 import { PostDetails } from './components/PostDetails';
 import { UserSelector } from './components/UserSelector';
 import { Loader } from './components/Loader';
-import { getUserPosts } from './api/posts';
-import { User } from './types/User';
-import { Post } from './types/Post';
+import { fetchUsers } from './features/users/usersSlice';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import * as postsActions from './features/posts/postsSlice';
+import * as selectedPostsActions from './features/selectedPost/selectedPost';
 
 export const App: React.FC = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [loaded, setLoaded] = useState(false);
-  const [hasError, setError] = useState(false);
+  const dispatch = useAppDispatch();
+  const author = useAppSelector(s => s.author);
+  const selectedPost = useAppSelector(s => s.selectedPost);
+  const { items: posts, loaded, hasError } = useAppSelector(s => s.posts);
 
-  const [author, setAuthor] = useState<User | null>(null);
-  const [selectedPost, setSelectedPost] = useState<Post | null>(null);
-
-  function loadUserPosts(userId: number) {
-    setLoaded(false);
-
-    getUserPosts(userId)
-      .then(setPosts)
-      .catch(() => setError(true))
-      // We disable the spinner in any case
-      .finally(() => setLoaded(true));
-  }
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]); // fetch all users
 
   useEffect(() => {
     // we clear the post when an author is changed
     // not to confuse the user
-    setSelectedPost(null);
+    dispatch(selectedPostsActions.clearSelectedPost());
 
     if (author) {
-      loadUserPosts(author.id);
+      dispatch(postsActions.fetchUserPosts(author.id));
     } else {
-      setPosts([]);
+      dispatch(postsActions.clearPosts());
     }
-  }, [author]);
+  }, [author, dispatch]);
 
   return (
     <main className="section">
@@ -50,14 +42,12 @@ export const App: React.FC = () => {
           <div className="tile is-parent">
             <div className="tile is-child box is-success">
               <div className="block">
-                <UserSelector value={author} onChange={setAuthor} />
+                <UserSelector />
               </div>
 
               <div className="block" data-cy="MainContent">
                 {!author && <p data-cy="NoSelectedUser">No user selected</p>}
-
                 {author && !loaded && <Loader />}
-
                 {author && loaded && hasError && (
                   <div
                     className="notification is-danger"
@@ -66,7 +56,6 @@ export const App: React.FC = () => {
                     Something went wrong!
                   </div>
                 )}
-
                 {author && loaded && !hasError && posts.length === 0 && (
                   <div className="notification is-warning" data-cy="NoPostsYet">
                     No posts yet
@@ -74,16 +63,11 @@ export const App: React.FC = () => {
                 )}
 
                 {author && loaded && !hasError && posts.length > 0 && (
-                  <PostsList
-                    posts={posts}
-                    selectedPostId={selectedPost?.id}
-                    onPostSelected={setSelectedPost}
-                  />
+                  <PostsList />
                 )}
               </div>
             </div>
           </div>
-
           <div
             data-cy="Sidebar"
             className={classNames(
@@ -97,7 +81,7 @@ export const App: React.FC = () => {
             )}
           >
             <div className="tile is-child box is-success ">
-              {selectedPost && <PostDetails post={selectedPost} />}
+              {selectedPost && <PostDetails />}
             </div>
           </div>
         </div>
