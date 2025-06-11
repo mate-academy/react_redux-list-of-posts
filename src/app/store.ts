6a0dfbec312b3fdb4b66f1fb21bf24584a1b3a6c
Olena7202 <olenahorodn7202@gmail.com>
2025-06-11 12:11:03 +0300

    Add task solution

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,16 +1,23 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 // eslint-disable-next-line import/no-cycle
 import counterReducer from '../features/counter/counterSlice';
+import authorReducer from '../features/author/autorSlice';
+import postsReducer from '../features/posts/postsSlice';
+import selectedPostReducer from '../features/selectedPost/selectedPostSlide';
+import usersReducer from '../features/users/usersSlide';
 
 export const store = configureStore({
   reducer: {
+    author: authorReducer,
+    posts: postsReducer,
+    selectedPost: selectedPostReducer,
     counter: counterReducer,
+    users: usersReducer,
   },
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
-
 /* eslint-disable @typescript-eslint/indent */
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
@@ -18,4 +25,3 @@ export type AppThunk<ReturnType = void> = ThunkAction<
   unknown,
   Action<string>
 >;
-/* eslint-enable @typescript-eslint/indent */
---
+++ src/features/author/autorSlice.tsx
@@ -0,0 +1,23 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { User } from '../../types/User';
+
+export interface AuthorState {
+  user: User | null;
+}
+
+const initialState: AuthorState = {
+  user: null,
+};
+
+const authorSlice = createSlice({
+  name: 'author',
+  initialState,
+  reducers: {
+    setAuthor: (state, action: PayloadAction<User | null>) => {
+      return { ...state, user: action.payload };
+    },
+  },
+});
+
+export const { setAuthor } = authorSlice.actions;
+export default authorSlice.reducer;
