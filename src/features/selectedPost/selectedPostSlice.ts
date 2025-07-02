f2b02e95b88b5ae71419bd4ae6e004ffaa37a613
Bernavschi Natalia <natb@list.ru>
2025-07-02 19:50:19 +0300

    add task solution for react_redux-list-of-posts

---
+++ src/features/selectedPost/selectedPostSlice.ts
@@ -0,0 +1,24 @@
+/* eslint-disable no-param-reassign */
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Post } from '../../types/Post';
+import { SelectedPostState } from '../../types/types';
+
+const initialState: SelectedPostState = {
+  current: null,
+};
+
+const selectedPostSlice = createSlice({
+  name: 'selectedPost',
+  initialState,
+  reducers: {
+    setSelectedPost: (state, action: PayloadAction<Post | null>) => {
+      state.current = action.payload;
+    },
+    clearSelectedPost: state => {
+      state.current = null;
+    },
+  },
+});
+
+export const { setSelectedPost, clearSelectedPost } = selectedPostSlice.actions;
+export default selectedPostSlice.reducer;
