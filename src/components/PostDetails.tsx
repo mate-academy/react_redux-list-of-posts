16ac935a54399396d8e33a1c5e44f706a4a52c39
unknown <vladhafarov@gmail.com>
2025-07-16 15:22:29 +0300

    fixed

--- src/components/PostDetails.tsx
+++ src/components/PostDetails.tsx
@@ -1,3 +1,124 @@
+// import React, { useEffect, useState } from 'react';
+// import { Loader } from './Loader';
+// import { NewCommentForm } from './NewCommentForm';
+// import * as commentsApi from '../api/comments';
+
+// import { Post } from '../types/Post';
+// import { CommentData } from '../types/Comment';
+// import { useAppDispatch, useAppSelector } from '../app/hooks';
+// import {
+//   initComment,
+//   removeComment,
+//   setComment,
+// } from '../features/comments/comment';
+
+// type Props = {
+//   post: Post;
+// };
+
+// export const PostDetails: React.FC<Props> = ({ post }) => {
+//   const dispatch = useAppDispatch();
+//   const { comments, loading, hasError } = useAppSelector(
+//     state => state.comments,
+//   );
+
+//   const [visible, setVisible] = useState(false);
+
+//   useEffect(() => {
+//     setVisible(false);
+
+//     dispatch(initComment(post.id));
+//   }, [post.id, dispatch]);
+
+//   const addComment = async ({ name, email, body }: CommentData) => {
+//     const newComment = await commentsApi.createComment({
+//       name,
+//       email,
+//       body,
+//       postId: post.id,
+//     });
+
+//     dispatch(setComment(newComment));
+//   };
+
+//   const deleteComment = async (commentId: number) => {
+//     await commentsApi.deleteComment(commentId);
+
+//     dispatch(removeComment(commentId));
+//   };
+
+//   return (
+//     <div className="content" data-cy="PostDetails">
+//       <div className="block">
+//         <h2 data-cy="PostTitle">{`#${post.id}: ${post.title}`}</h2>
+//         <p data-cy="PostBody">{post.body}</p>
+//       </div>
+
+//       <div className="block">
+//         {!loading && <Loader />}
+
+//         {loading && hasError && (
+//           <div className="notification is-danger" data-cy="CommentsError">
+//             Something went wrong
+//           </div>
+//         )}
+
+//         {loading && !hasError && comments.length === 0 && (
+//           <p className="title is-4" data-cy="NoCommentsMessage">
+//             No comments yet
+//           </p>
+//         )}
+
+//         {loading && !hasError && comments.length > 0 && (
+//           <>
+//             <p className="title is-4">Comments:</p>
+
+//             {comments.map(comment => (
+//               <article
+//                 className="message is-small"
+//                 key={comment.id}
+//                 data-cy="Comment"
+//               >
+//                 <div className="message-header">
+//                   <a href={`mailto:${comment.email}`} data-cy="CommentAuthor">
+//                     {comment.name}
+//                   </a>
+//                   <button
+//                     data-cy="CommentDelete"
+//                     type="button"
+//                     className="delete is-small"
+//                     aria-label="delete"
+//                     onClick={() => deleteComment(comment.id)}
+//                   ></button>
+//                 </div>
+
+//                 <div className="message-body" data-cy="CommentBody">
+//                   {comment.body}
+//                 </div>
+//               </article>
+//             ))}
+//           </>
+//         )}
+
+//         {loading && !hasError && !visible && (
+//           <button
+//             data-cy="WriteCommentButton"
+//             type="button"
+//             className="button is-link"
+//             onClick={() => setVisible(true)}
+//           >
+//             Write a comment
+//           </button>
+//         )}
+
+//         {loading && !hasError && visible && (
+//           <NewCommentForm onSubmit={addComment} />
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
 import React, { useEffect, useState } from 'react';
 import { Loader } from './Loader';
 import { NewCommentForm } from './NewCommentForm';
@@ -55,21 +176,21 @@ export const PostDetails: React.FC<Props> = ({ post }) => {
       </div>
 
       <div className="block">
-        {!loading && <Loader />}
+        {loading && <Loader />}
 
-        {loading && hasError && (
+        {!loading && hasError && (
           <div className="notification is-danger" data-cy="CommentsError">
             Something went wrong
           </div>
         )}
 
-        {loading && !hasError && comments.length === 0 && (
+        {!loading && !hasError && comments.length === 0 && (
           <p className="title is-4" data-cy="NoCommentsMessage">
             No comments yet
           </p>
         )}
 
-        {loading && !hasError && comments.length > 0 && (
+        {!loading && !hasError && comments.length > 0 && (
           <>
             <p className="title is-4">Comments:</p>
 
@@ -100,7 +221,7 @@ export const PostDetails: React.FC<Props> = ({ post }) => {
           </>
         )}
 
-        {loading && !hasError && !visible && (
+        {!loading && !hasError && !visible && (
           <button
             data-cy="WriteCommentButton"
             type="button"
@@ -111,7 +232,7 @@ export const PostDetails: React.FC<Props> = ({ post }) => {
           </button>
         )}
 
-        {loading && !hasError && visible && (
+        {!loading && !hasError && visible && (
           <NewCommentForm onSubmit={addComment} />
         )}
       </div>
