fd6f60c10b128b21da93adc87506ae1edc13f60b
Ana <anastasia.skripka.sao@gmail.com>
2025-08-03 17:29:29 +0200

    add solution

---
+++ src/features/posts/postsSlice.ts
@@ -0,0 +1,54 @@
+/* eslint-disable no-param-reassign */
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
+import { getUserPosts } from '../../api/posts';
+import { Post } from '../../types/Post';
+import { PostsState } from '../../types/types';
+
+const initialState: PostsState = {
+  posts: [],
+  loaded: false,
+  hasError: false,
+};
+
+export const fetchUserPosts = createAsyncThunk(
+  'posts/fetchUserPosts',
+  async (userId: number) => {
+    const posts = await getUserPosts(userId);
+
+    return posts;
+  },
+);
+
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {
+    clearPosts: state => {
+      state.posts = [];
+      state.loaded = false;
+      state.hasError = false;
+    },
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(fetchUserPosts.pending, state => {
+        state.loaded = false;
+        state.hasError = false;
+      })
+      .addCase(
+        fetchUserPosts.fulfilled,
+        (state, action: PayloadAction<Post[]>) => {
+          state.loaded = true;
+          state.hasError = false;
+          state.posts = action.payload;
+        },
+      )
+      .addCase(fetchUserPosts.rejected, state => {
+        state.loaded = true;
+        state.hasError = true;
+      });
+  },
+});
+
+export const { clearPosts } = postsSlice.actions;
+export default postsSlice.reducer;
