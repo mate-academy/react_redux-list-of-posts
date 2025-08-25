b50f183cef8556d0cfa1912a0c35958d6b8e8839
Vlad Mitlosh <vlad.mitlosh@gmail.com>
2025-08-25 15:54:02 +0300

    solution

--- src/components/UserSelector.tsx
+++ src/components/UserSelector.tsx
@@ -1,24 +1,20 @@
-import React, { useContext, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import classNames from 'classnames';
-import { UserContext } from './UsersContext';
-import { User } from '../types/User';
+import { useAppDispatch, useAppSelector } from '../app/hooks';
+import { fetchUsers } from '../features/usersSlice';
+import { setAuthor } from '../features/authorSlice';
 
-type Props = {
-  value: User | null;
-  onChange: (user: User) => void;
-};
-
-export const UserSelector: React.FC<Props> = ({
-  // `value` and `onChange` are traditional names for the form field
-  // `selectedUser` represents what actually stored here
-  value: selectedUser,
-  onChange,
-}) => {
-  // `users` are loaded from the API, so for the performance reasons
-  // we load them once in the `UsersContext` when the `App` is opened
-  // and now we can easily reuse the `UserSelector` in any form
-  const users = useContext(UserContext);
+export const UserSelector = () => {
+  const users = useAppSelector(state => state.users.users);
+  const dispatch = useAppDispatch();
+  const selectedUser = useAppSelector(state => state.author.current);
   const [expanded, setExpanded] = useState(false);
+
+  useEffect(() => {
+    if (!users.length) {
+      dispatch(fetchUsers());
+    }
+  }, [dispatch, users.length]);
 
   useEffect(() => {
     if (!expanded) {
@@ -73,7 +69,8 @@ export const UserSelector: React.FC<Props> = ({
               key={user.id}
               href={`#user-${user.id}`}
               onClick={() => {
-                onChange(user);
+                dispatch(setAuthor(user));
+                setExpanded(false);
               }}
               className={classNames('dropdown-item', {
                 'is-active': user.id === selectedUser?.id,
---
+++ src/features/authorSlice.ts
@@ -0,0 +1,25 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { User } from '../types/User';
+
+export interface AuthorState {
+  current: User | null;
+}
+
+const initialState: AuthorState = {
+  current: null,
+};
+
+const authorSlice = createSlice({
+  name: 'author',
+  initialState,
+  reducers: {
+    setAuthor: (_state, action: PayloadAction<User | null>) => {
+      return {
+        current: action.payload,
+      };
+    },
+  },
+});
+
+export const { setAuthor } = authorSlice.actions;
+export default authorSlice.reducer;
