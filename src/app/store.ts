236a6452646f47b7e21aaa017c6724150b389539
diego69-foda <diego.bkuhn@gmail.com>
2025-12-09 10:02:23 -0300

    feat: refactor App component to use Redux for state management and update store configuration

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,17 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import usersReducer from '../features/users/usersSlice';
+import authorReducer from '../features/author/authorSlice';
+import postsReducer from '../features/posts/postsSlice';
+import selectedPostReducer from '../features/selectedPost/selectedPostSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     users: usersReducer,
+    author: authorReducer,
+    posts: postsReducer,
+    selectedPost: selectedPostReducer,
   },
 });
 
