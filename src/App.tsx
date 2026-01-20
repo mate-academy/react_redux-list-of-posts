3fef18c40a399d02d4d4561ea8e7da9b672dc966
Viktoriia <vikapazyuk@gmail.com>
2026-01-20 20:50:30 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import classNames from 'classnames';
+import { useDispatch, useSelector } from 'react-redux';
 
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
@@ -9,39 +10,51 @@ import { PostsList } from './components/PostsList';
 import { PostDetails } from './components/PostDetails';
 import { UserSelector } from './components/UserSelector';
 import { Loader } from './components/Loader';
+
+import { RootState, AppDispatch } from './app/store';
+import { setAuthor } from './features/author/authorSlice';
+import { setPosts, setLoaded, setHasError } from './features/posts/postsSlice';
+import { setSelectedPost } from './features/selectedPost/selectedPostSlice';
+
 import { getUserPosts } from './api/posts';
 import { User } from './types/User';
-import { Post } from './types/Post';
 
 export const App: React.FC = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [loaded, setLoaded] = useState(false);
-  const [hasError, setError] = useState(false);
+  const dispatch = useDispatch<AppDispatch>();
 
-  const [author, setAuthor] = useState<User | null>(null);
-  const [selectedPost, setSelectedPost] = useState<Post | null>(null);
+  // Достаём данные из Redux
+  const author = useSelector((state: RootState) => state.author.selected);
+  const posts = useSelector((state: RootState) => state.posts.items);
+  const loaded = useSelector((state: RootState) => state.posts.loaded);
+  const hasError = useSelector((state: RootState) => state.posts.hasError);
+  const selectedPost = useSelector(
+    (state: RootState) => state.selectedPost.post,
+  );
 
+  // Загрузка постов по userId
   function loadUserPosts(userId: number) {
-    setLoaded(false);
+    dispatch(setLoaded(false));
 
     getUserPosts(userId)
-      .then(setPosts)
-      .catch(() => setError(true))
-      // We disable the spinner in any case
-      .finally(() => setLoaded(true));
+      .then(postsArray => {
+        dispatch(setPosts(postsArray));
+        dispatch(setHasError(false));
+      })
+      .catch(() => dispatch(setHasError(true)))
+      .finally(() => dispatch(setLoaded(true)));
   }
 
-  useEffect(() => {
-    // we clear the post when an author is changed
-    // not to confuse the user
-    setSelectedPost(null);
+  // Когда выбираем нового юзера
+  function handleUserChange(user: User | null) {
+    dispatch(setAuthor(user));
+    dispatch(setSelectedPost(null));
 
-    if (author) {
-      loadUserPosts(author.id);
+    if (user) {
+      loadUserPosts(user.id);
     } else {
-      setPosts([]);
+      dispatch(setPosts([]));
     }
-  }, [author]);
+  }
 
   return (
     <main className="section">
@@ -50,7 +63,7 @@ export const App: React.FC = () => {
           <div className="tile is-parent">
             <div className="tile is-child box is-success">
               <div className="block">
-                <UserSelector value={author} onChange={setAuthor} />
+                <UserSelector value={author} onChange={handleUserChange} />
               </div>
 
               <div className="block" data-cy="MainContent">
@@ -77,7 +90,7 @@ export const App: React.FC = () => {
                   <PostsList
                     posts={posts}
                     selectedPostId={selectedPost?.id}
-                    onPostSelected={setSelectedPost}
+                    onPostSelected={post => dispatch(setSelectedPost(post))}
                   />
                 )}
               </div>
---
+++ src/features/selectedPost/selectedPostSlice.ts
@@ -0,0 +1,25 @@
+/* eslint-disable no-param-reassign */
+
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Post } from '../../types/Post';
+
+export interface SelectedPostState {
+  post: Post | null;
+}
+
+const initialState: SelectedPostState = {
+  post: null,
+};
+
+export const selectedPostSlice = createSlice({
+  name: 'selectedPost',
+  initialState,
+  reducers: {
+    setSelectedPost: (state, action: PayloadAction<Post | null>) => {
+      state.post = action.payload;
+    },
+  },
+});
+
+export const { setSelectedPost } = selectedPostSlice.actions;
+export default selectedPostSlice.reducer;
